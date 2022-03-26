6e1ebe340f5eb855c7f13f99d808e1544bb73a90
jannatnury <jannatnury253@gmail.com>
2022-03-26 12:17:59 +0600

    second commit

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+    const handleCart=()=>{
+        console.log('clicked');
+    }
     const {seller,name,price,img,ratings}=props.product;
     return (
         <div className='products'>
             <img src={img} alt="" />
-            <div className='details-container'>
+            <div className='product-details'>
             <h4>Name:{name}</h4>
-            <p>Price:${price}</p>
+            <p>Price: ${price}</p>
             <p><small>Manufecturer:{seller}</small></p>
             <p><small>Ratings:{ratings}</small></p>
             </div>
+            <button onClick={handleCart} className='product-btn'>Add to cart</button>
         </div>
     );
 };
